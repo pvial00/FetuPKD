aa73fd3a540dbcc3da09dd33523c44a4563efda2
Karl Zander <pvial@Karls-MacBook-Pro.local>
2021-04-16 13:04:05 -0700

    FetuI and FetuX

--- fetuX.py
+++ fetuX.py
@@ -1,7 +1,7 @@
 from Crypto.Util import number
 
 ''' KryptoMagick 2021 '''
-''' This code demonstrates the Fetu X algorithm '''
+''' This code demonstrates the reduced Fetu X algorithm '''
 
 def genBase(size):
     A = number.getPrime(size)
@@ -15,36 +15,31 @@ def keygen(size):
     sk = number.getRandomRange(1, (M - 1))
     return sk, N, M
 
-def fetu_demo(size):
+def fetu_demo(size, itera=100000):
     print("Generating Alice and Bob's keys")
     skA, nA, MA = keygen(size)
     skB, nB, MB = keygen(size)
+    T = 2
     print(skA, nA, MA)
     print(skB, nB, MB)
     print("Exchanging and agreeing upon a public modulus")
-    print("Each choose a number in the public modulus")
-    y = number.getRandomRange(1, (MA - 1))
-    yB = number.getRandomRange(1, (MA - 1))
-    print(y, yB)
-    print("Each choose a temporary key in the public modulus")
-    TkA = number.getRandomRange(1, (MA - 1))
-    TkB = number.getRandomRange(1, (MA - 1))
-    print(TkA, TkB)
     print("Compute phase0")
-    p0 = pow(nA, TkA, MA)
-    p0B = pow(nA, TkB, MA)
+    p0 = pow(nA, skA, MA)
+    p0B = pow(nA, skB, MA)
     print(p0, p0B)
-    print("Compute phase1")
-    p1 = pow(p0, y, MA)
-    p1B = pow(p0B, nB, MA)
-    print(p1, p1B)
-    print("Exchange phase2")
-    p2 = pow(p1, nB, MA)
-    p2B = pow(p1B, y, MA)
+    print("Exchange phase1")
+    p1 = pow(p0, T, MA)
+    p1B = pow(p0B, T, MA)
+    for x in range(itera):
+        p1 = pow(p1, T, MA)
+        p1B = pow(p1B, T, MA)
+        print(p1, p1B)
+    p2 = pow(p1, nA, MA)
+    p2B = pow(p1B, nA, MA)
     print(p2, p2B)
-    print("Arrive at the secret key phase3")
-    p3 = pow(p2B, TkA, MA)
-    p3B = pow(p2, TkB, MA)
+    print("Arrive at the secret key phase2")
+    p3 = pow(p2B, skA, MA)
+    p3B = pow(p2, skB, MA)
     print(p3, p3B)
 
-fetu_demo(16)
+fetu_demo(256)
